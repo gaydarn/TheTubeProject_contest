480fce8bbde3343bcc4d1e8eb8d91b61cdfb4a43
Gay des Combes Arnaud <arnaud.gaydesco@he-arc.ch>
2023-04-19 11:23:17 +0200

    Ajout gestion des enregistrements et statistiques

--- Client_Standalone.py
+++ Client_Standalone.py
@@ -1,13 +1,15 @@
 # Press Maj+F10 to execute
 
 # Importation des librairie
-
+import os
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 import datetime, threading, time
 import serial
 import Util
+from scipy import integrate
+import sys
 
 
 TrajIndex = 0
@@ -30,11 +32,11 @@ if __name__ == '__main__':
 
     print('Création du Thread de lecture des données de l\'arduino')
     ReadThread = threading.Thread(target=Util.readToBuffer, args=(arduino, readingBuffer), daemon=True)
-    print('Création du Thread d\'écriture des informations de trajectoires')
+    print('Création du Thread d\'écriture des informations de trajectoire')
     writeThread = threading.Thread(target=Util.trajectory_generation, args=(arduino, TrajIndex, TrajLen, TrajData, stop_event), daemon=True)
 
     print('Passage de l\'Arduino dans le mode du concours')
-    Util.write(arduino,'contest')
+    Util.write(arduino, 'contest')
     time.sleep(10)
 
     print('Démarrage des threads')
@@ -42,15 +44,43 @@ if __name__ == '__main__':
     writeThread.start()
 
     print('Concours en cours...')
-    time.sleep(5) #TODO: Start/stop from server?
+    time.sleep(2*60) #TODO: Start/stop from server?
 
     print('Trajectoire terminée, arrêt du thread d\'écriture et passage de l\'Arduino en mode "Quiet"')
     stop_event.set()
     writeThread.join()
-    Util.write(arduino,'contest') #Permet de forcer la consigne de départ à 0
+    Util.write(arduino, 'contest') #Permet de forcer la consigne de départ à 0
     time.sleep(2)
-    Util.write(arduino,'quiet')
+    Util.write(arduino, 'quiet')
 
+    print('Formattage du résultat final et calcul des statistiques')
     df_final = Util.formatFinalBufferintoDataFrame(readingBuffer)
     df_final.plot()
+    fig_final = plt.gcf()
     plt.show()
+
+    #Statistiques
+    df_final['following_err'] = np.abs(df_final.position-df_final.setpoint)
+    df_final['t_ms'] = df_final.index.values.astype(np.int64) // 10 ** 6
+    df_final.t_ms = df_final.t_ms.values.astype(np.int64) - np.int64(df_final.t_ms[0])
+
+    avg_int = integrate.trapz(df_final.following_err, x=df_final.t_ms.values) / np.int64(df_final.t_ms[-1])
+    max_foll_err = np.max(df_final.following_err)
+    avg_foll_err = np.mean(df_final.following_err)
+
+    # Saving the different results into files
+    rep_base = 'LastRun'
+
+    df_final.to_csv(os.path.join(os.getcwd(), rep_base, 'final_traj.csv'))
+    fig_final.savefig(os.path.join(os.getcwd(), rep_base, 'final_traj.png'))
+
+    stats =  '***** Statistiques de l\'erreur de poursuite *****\r\n' \
+            f'Moyenne : {avg_foll_err} mm\r\n' \
+            f'Moyenne de l\'intégrale : {avg_int} mm\r\n' \
+            f'Maximum : {max_foll_err} mm\r\n' \
+
+    with open(os.path.join(os.getcwd(), rep_base, 'final_traj_stat.txt'), 'w') as f:
+        f.write(stats)
+
+
+
